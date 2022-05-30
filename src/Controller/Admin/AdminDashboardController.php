095eb4de9553b2fe09dea0a2bf31a6e149346922
gerardo montivero <gerardo.montivero@gmail.com>
2022-05-30 10:43:37 -0300

    Lista la vista

--- src/Controller/Admin/AdminDashboardController.php
+++ src/Controller/Admin/AdminDashboardController.php
@@ -6,6 +6,7 @@ use App\Entity\Corazon;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,7 +29,9 @@ class AdminDashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+        return $this->redirect($routeBuilder->setController(CorazonCrudController::class)->generateUrl());
+
     }
 
     public function configureDashboard(): Dashboard
