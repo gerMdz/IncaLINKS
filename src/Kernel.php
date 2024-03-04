6c151a41ba53c2f87bea8bdb279f0d078ff7608d
germdz <gerardo.montivero@gmail.com>
2024-03-04 19:40:13 -0300

    Empezamos rector 1, fix 1, recipes 1 framework-bundle

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,21 +12,9 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
-        $container->import('../config/{services}.yaml');
-        $container->import('../config/{services}_'.$this->environment.'.yaml');
-    }
-
-
 }
